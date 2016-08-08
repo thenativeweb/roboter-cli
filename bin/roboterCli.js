50672b03b09254d37479c89fba9e6cb4074241ce
Golo Roden <webmaster@goloroden.de>
2016-08-08 16:42:24 +0200

    Fix missing usage message. Close thenativeweb/org-dev#276

--- bin/roboterCli.js
+++ bin/roboterCli.js
@@ -9,6 +9,29 @@ const buntstift = require('buntstift'),
       minimist = require('minimist'),
       shell = require('shelljs');
 
+if (process.argv[2] === '--help') {
+  buntstift.info('roboter  - automate your builds');
+  buntstift.newLine();
+  buntstift.info('Usage:');
+  buntstift.info('  bot [command]');
+  buntstift.newLine();
+  buntstift.info('Available Commands:');
+  buntstift.info('  analyse            run static code analysis');
+  buntstift.info('  build-client       build a client application');
+  buntstift.info('  build-server       build a server application');
+  buntstift.info('  coverage           calculate test coverage');
+  buntstift.info('  license            verify licenses');
+  buntstift.info('  outdated           detect outdated packages');
+  buntstift.info('  publish            publish a new version');
+  buntstift.info('  test               run unit and integration tests');
+  buntstift.info('  test-integration   run integration tests');
+  buntstift.info('  test-units         run unit tests');
+  buntstift.info('  update             update outdated packages');
+  buntstift.info('  watch-client       continuously build a client application');
+  buntstift.info('  watch-server       continuously build a server application');
+  buntstift.exit(0);
+}
+
 /* eslint-disable no-sync */
 if (!fs.existsSync(path.join(process.cwd(), 'node_modules', 'roboter'))) {
   buntstift.error('roboter is not installed locally.');
